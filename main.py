30273e57bc6a669c7769f82a381985c22cfb2d52
Arek Sanecki <areksanecki@gmail.com>
2023-04-13 19:00:16 +0200

    root

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from tkinter import *
 def main():
-    pass
+    root=Tk()
+    root.title('GRA "PAPIER, KAMIEŃ, NOŻYCZKI"')
+    root.iconbitmap("logo.ico")
+    root.geometry("900x900")
 
+
+
+    root.mainloop()
 if __name__ == '__main__':
     main()
